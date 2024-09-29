dcb6d5b35cb69f82d900738c18ea5f65b515e3cc
DonSlon1 <lukindihel@gmail.com>
2024-09-29 22:23:38 +0200

    working picture taking

--- app/src/main/java/com/example/smartinventory/ui/addwarehouseitem/view/AddWarehouseItemFragment.kt
+++ app/src/main/java/com/example/smartinventory/ui/addwarehouseitem/view/AddWarehouseItemFragment.kt
@@ -1,72 +1,118 @@
 package com.example.smartinventory.ui.addwarehouseitem.view
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.lifecycleScope
 import com.example.smartinventory.data.model.InventoryItem
 import com.example.smartinventory.viewmodel.addwarehouseaction.AddWarehouseActionViewModel
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Date
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class AddWarehouseItemFragment: Fragment() {
-/*
-    private var _binding: FragmentAddWarehouseItemBinding? = null
-    private val binding get() = _binding!!
+class AddWarehouseItemFragment @Inject constructor(
+):  Fragment() {
 
-    private val viewModel: AddWarehouseActionViewModel by viewModels()
-    val allItems by viewModel.filteredInventoryItem.observeAsState(emptyList())
+    companion object {
+        private const val CAMERA_REQUEST_CODE = 1001
+    }
 
-*/
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                AddWarehouseItemScreen()
+                val launcher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
+                    if (bitmap != null) {
+                        val image = InputImage.fromBitmap(bitmap, 0)
+                        scanBarcodes(image) { barcodes ->
+                            if (barcodes.isNotEmpty()) {
+                                val barcodeResult = barcodes.first().rawValue.orEmpty()
+                                Log.d("AddWarehouseItemFragment", "Barcode detected: $barcodeResult")
+                                // Update the UI or handle the result as needed
+                            }
+                        }
+                    }
+                }
+
+                AddWarehouseItemScreenWithViewModel(
+                    viewModel = hiltViewModel(),
+                    onItemClicked = { item ->
+                        Log.d("AddWarehouseItemFragment", "Item clicked: ${item.name}")
+                    },
+                    onCaptureButtonClick = {
+                        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                            launcher.launch(null)
+                        } else {
+                            ActivityCompat.requestPermissions(activity as Activity, arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+                        }
+                    },
+                )
             }
+        }
+    }
+    private fun scanBarcodes(image: InputImage, onSuccess: (List<Barcode>) -> Unit) {
+        val options = BarcodeScannerOptions.Builder().setBarcodeFormats(
+            Barcode.FORMAT_CODE_128
+        ).build()
+        val scanner =
+            BarcodeScanning.getClient(options)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+           scanner.process(image)
+                .addOnSuccessListener { barcodes ->
+                    onSuccess(barcodes)
+                }
+                .addOnFailureListener { e ->
+                    Log.e("BarcodeScan", "Barcode scanning failed", e)
+                }
         }
     }
 }
 
+
 @Composable
-fun AddWarehouseItemScreen(viewModel: AddWarehouseActionViewModel = hiltViewModel()) {
-
-    val allItems by viewModel.filteredInventoryItem.observeAsState(emptyList())
-    var filterText by remember { mutableStateOf("") }
-    val coroutineScope = rememberCoroutineScope()
-
+fun AddWarehouseItemScreen(
+    filterText: String,
+    onFilterTextChange: (String) -> Unit,
+    onScanBarcodeClick: () -> Unit,
+    items: List<InventoryItem>,
+    onItemClicked: (InventoryItem) -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -75,10 +121,7 @@ fun AddWarehouseItemScreen(viewModel: AddWarehouseActionViewModel = hiltViewMode
         // Search TextField
         OutlinedTextField(
             value = filterText,
-            onValueChange = {
-                filterText = it
-                viewModel.filterItems(it)
-            },
+            onValueChange = onFilterTextChange,
             label = { Text("Search Items") },
             modifier = Modifier.fillMaxWidth()
         )
@@ -87,10 +130,7 @@ fun AddWarehouseItemScreen(viewModel: AddWarehouseActionViewModel = hiltViewMode
 
         // Scan Barcode Button
         Button(
-            onClick = {
-                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                Log.d("MainScreen", "Launching camera intent")
-            },
+            onClick = onScanBarcodeClick,
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Scan Barcode")
@@ -102,30 +142,92 @@ fun AddWarehouseItemScreen(viewModel: AddWarehouseActionViewModel = hiltViewMode
         LazyColumn(
             modifier = Modifier.fillMaxSize()
         ) {
-            items(allItems) { item ->
-                WarehouseItemRow(item)
-                HorizontalDivider(thickness = 1.dp, color = Color.Gray)
+            items(items) { item ->
+                WarehouseItemRow(item = item, onItemClicked = onItemClicked)
             }
         }
     }
 }
 
 @Composable
-fun WarehouseItemRow(item: InventoryItem) {
+fun WarehouseItemRow(item: InventoryItem, onItemClicked: (InventoryItem) -> Unit) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
-            .clickable {
-                Log.d("WarehouseItemRow", "Item clicked: ${item.name}")
-            },
+            .clickable { onItemClicked(item) }
+            .padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Column {
-            Text(text = item.name)
-            Text(text = "Type: ${item.category}")
-            Text(text = "Status: ${item.quantity}")
-            Text(text = "Barcode: ${item.ean}")
+            Text(text = item.name, style = MaterialTheme.typography.bodyLarge)
+            Text(text = "Type: ${item.category}", style = MaterialTheme.typography.bodySmall)
+            Text(text = "Quantity: ${item.quantity}", style = MaterialTheme.typography.bodySmall)
+            Text(text = "Barcode: ${item.ean}", style = MaterialTheme.typography.bodySmall)
         }
     }
+}
+
+@Composable
+fun AddWarehouseItemScreenWithViewModel(
+    viewModel: AddWarehouseActionViewModel,
+    onItemClicked: (InventoryItem) -> Unit = {},
+    onCaptureButtonClick: () -> Unit,
+
+    ) {
+    val filterText by viewModel.filterQuery.observeAsState("")
+    val items by viewModel.filteredInventoryItem.observeAsState(emptyList())
+
+    val context = LocalContext.current
+
+    // Define the Filter Text Change Handler
+    val onFilterTextChange: (String) -> Unit = { query ->
+        viewModel.filterItems(query)
+    }
+
+    AddWarehouseItemScreen(
+        filterText = filterText,
+        onFilterTextChange = onFilterTextChange,
+        onScanBarcodeClick = onCaptureButtonClick,
+        items = items,
+        onItemClicked = onItemClicked
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AddWarehouseItemScreenPreview() {
+    val sampleItems = listOf(
+        InventoryItem(
+            name = "Sample Item 1",
+            category = "Sample Category A",
+            quantity = 20,
+            ean = 11122,
+            expiryDate = Date(),
+            imageUrl = null,
+            sku = "SKU-001",
+            supplier = "Supplier X",
+            reorderLevel = 5,
+            unitPrice = 99.99
+        ),
+        InventoryItem(
+            name = "Sample Item 2",
+            category = "Sample Category B",
+            quantity = 30,
+            ean = 55566,
+            expiryDate = Date(),
+            imageUrl = null,
+            sku = "SKU-002",
+            supplier = "Supplier Y",
+            reorderLevel = 10,
+            unitPrice = 149.99
+        )
+    )
+
+    AddWarehouseItemScreen(
+        filterText = "",
+        onFilterTextChange = {},
+        onScanBarcodeClick = {},
+        items = sampleItems,
+        onItemClicked = {}
+    )
 }
